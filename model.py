b8c2c7fb603fdcd83c990040819dfa6bb848fe5c
Amy <amy.mae.lee@gmail.com>
2014-03-06 17:15:09 -0800

    Part 1

--- model.py
+++ model.py
@@ -1,13 +1,63 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+import datetime
 
+ENGINE = None
+Session = None
+
+# This is what we use to later declare a class to be managed by SQLAlchemy
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+    # data = relationship("Data", order_by = movies_id")
+
+class Ratingsdata(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer)
+    user_id = Column(Integer)
+    rating = Column(Integer)
+
+# ForeignKey('movies.movie_id')
+# ForeignKey('users.id')
+    # user = relationship("User", backref=backref('data', order_by=id))
+    # movie = relationship("Movie", backref=backref('data', order_by=id))
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    movie_id = Column(Integer, primary_key=True)
+    title = Column(String(64), nullable=True)
+    released = Column(DateTime, nullable=True)
+    url = Column(String(64), nullable=True)
+
+    # data = relationship("Data", order_by = movies_id")
 
 
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
+
 
 def main():
     """In case we need this for something"""
@@ -15,3 +65,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# NOTE: This translate the above python metadata code to SQl to create the tables
+    # Once create, table = class, the attributes =  columns, row = instance
+# python -i model.py
+# engine = create_engine("sqlite:///ratings.db", echo=True)
+# Base.metadata.create_all(engine)
+
+
+# NOTE: This is similar to DB.connect() to connect to the database, equivalent to session
+# session = connect()
+
+# NOTE: to modify, must pull object out, then modify, then commit
+# c = session.query(User).get(1)
+# c.password = "somethingmoresecure"
+# session.commit()
+
+# NOTE: must create object, then be explicit about adding, then commit
+# session.add(charles)
+# session = connect()
+
+
+# datetime.datetime.strptime("01-Jan-1994", "%d-%b-%Y")
--- seed.py
+++ seed.py
@@ -1,22 +1,94 @@
 import model
 import csv
+import datetime
+import time
+
+
+session = model.connect()
+
+
+
+moviefile = open("seed_data/u.item") 
+
+for movieline in moviefile:  
+    movieinstance = movieline.split("|")
+    # print movieinstance
+    if movieinstance[2] == '':
+        # print movieinstance
+        # releasedate[2] = null
+        movieitem = model.Movie(movie_id = movieinstance[0], 
+                title = movieinstance[1],
+                released = datetime.datetime.strptime("01-Jan-1970", "%d-%b-%Y"),
+                url = "unknown")
+    else:         
+        releasedate = datetime.datetime.strptime(movieinstance[2], "%d-%b-%Y")
+        # print releasedate
+        movietitlelist = movieinstance[1].split(" (")
+        movietitle = movietitlelist[0]
+        movieitem = model.Movie(movie_id = movieinstance[0],
+                title = movietitle, 
+                released = releasedate, 
+                url = movieinstance[4])
+
+    session.add(movieitem)
+
+session.commit()
+userfile.close()
+
 
 def load_users(session):
-    # use u.user
-    pass
+    userfile = open("seed_data/u.user") 
+
+    for userline in userfile:  
+        userinstance = userline.split("|")
+        # print userinstance
+        # columnnames = ("id", "email", "password", "age", "zipcode")
+        insertitem = model.User(id = userinstance[0], email = userinstance[1], password = userinstance[2], age = userinstance[3], zipcode = userinstance[4])
+        session.add(insertitem)
+
+    session.commit()
+    userfile.close()
 
 def load_movies(session):
-    # use u.item
-    pass
+
+    moviefile = open("seed_data/u.item") 
+
+    for movieline in moviefile:  
+        movieinstance = movieline.split("|")
+        # print movieinstance
+        if movieinstance[2] == '':
+            # print movieinstance
+            # releasedate[2] = null
+            movieitem = model.Movie(movie_id = movieinstance[0], 
+                    title = movieinstance[1],
+                    released = datetime.datetime.strptime("01-Jan-1970", "%d-%b-%Y"),
+                    url = "unknown")
+        else:         
+            releasedate = datetime.datetime.strptime(movieinstance[2], "%d-%b-%Y")
+            # print releasedate
+            movietitlelist = movieinstance[1].split(" (")
+            movietitle = movietitlelist[0]
+            movieitem = model.Movie(movie_id = movieinstance[0],
+                    title = movietitle, 
+                    released = releasedate, 
+                    url = movieinstance[4])
+
+        session.add(movieitem)
+    
+    session.commit()
+    userfile.close()
 
 def load_ratings(session):
     # use u.data
     pass
+
+
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
     pass
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.connect()
     main(s)
